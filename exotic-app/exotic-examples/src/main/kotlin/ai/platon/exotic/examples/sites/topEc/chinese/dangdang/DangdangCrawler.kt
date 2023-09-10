a5da51d7ff8f6bbf1be7f59189ccccba835974f4
PlatonAI <galaxy.eye@qq.com>
2023-09-11 00:59:10 +0800

    Imps: 1. replace deprecated names to the replacement

--- exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/chinese/dangdang/DangdangCrawler.kt
+++ exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/topEc/chinese/dangdang/DangdangCrawler.kt
@@ -3,10 +3,10 @@ package ai.platon.exotic.examples.sites.topEc.chinese.dangdang
 import ai.platon.pulsar.context.PulsarContexts
 
 fun main() {
-    val portalUrl = "http://category.dangdang.com/cid4010209.html"
+    val portalUrl = "https://category.dangdang.com/cid4010209.html"
     val args = "-i 1s -ii 5d -ol a[href~=product] -ignoreFailure"
 
-    PulsarContexts.createSession().submitOutPages(portalUrl, args)
+    PulsarContexts.createSession().submitForOutPages(portalUrl, args)
 
     PulsarContexts.await()
 }
