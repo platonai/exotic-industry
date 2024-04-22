0434e87b811bc3ef4e6d0521a66ae985563a061d
PlatonAI <galaxy.eye@qq.com>
2024-04-22 11:40:11 +0800

    Imps: 1. download model from our server for DOMEvaluator

--- exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/Twitter.kt
+++ exotic-app/exotic-examples/src/main/kotlin/ai/platon/exotic/examples/sites/Twitter.kt
@@ -1,9 +1,11 @@
 package ai.platon.exotic.examples.sites
 
 import ai.platon.pulsar.browser.common.BrowserSettings
+import ai.platon.pulsar.browser.common.InteractSettings
 import ai.platon.pulsar.context.PulsarContexts
 
 fun main() {
+    BrowserSettings.interactSettings = InteractSettings.DEFAULT.noScroll()
     BrowserSettings.withSystemDefaultBrowser()
     val session = PulsarContexts.createSession()
     val args = "-i 7s -ii 7s"
@@ -11,11 +13,30 @@ fun main() {
     val url = "https://twitter.com/home"
     val options = session.options(args)
     val be = options.event.browseEvent
-    
+
     be.onDidScroll.addLast { page, driver ->
-        // close mask
+        // login
+        if (page.url.contains("login")) {
+//            driver.findElementByCssSelector("input[name='session[username_or_email]']").sendKeys("username")
+//            driver.findElementByCssSelector("input[name='session[password]']").sendKeys("password")
+//            driver.findElementByCssSelector("div[data-testid='LoginForm_Login_Button']").click()
+
+            val username = System.getProperty("TWITTER_USERNAME")
+            val password = System.getProperty("TWITTER_PASSWORD")
+            if (driver.visible("")) {
+                driver.type("input[name='session[username_or_email]']", username)
+                driver.type("input[name='session[username_or_email]']", password)
+                driver.click("div[data-testid='LoginForm_Login_Button']")
+            }
+
+            driver.waitForNavigation()
+        }
+        
+        driver.browser.drivers.forEach { url, driver ->
+            // println(driver.currentUrl)
+        }
     }
-    
+
     session.load(url, options)
     
     PulsarContexts.await()
